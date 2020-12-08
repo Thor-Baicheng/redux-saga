b7346599feea9e25658569e42c95ea24d588c84d
前端摸鱼欧阳锋 <718781500@qq.com>
2020-12-08 15:12:48 +0800

    编写了actions和types

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
 import { createStore, applyMiddleware } from "redux"
 
-import { composeWithDevTools } from "redux-devtools-extension"
+import { composeWithDevTools } from "redux-devtools-extension" //调试工具
 
-import rootReducer from "./reducers"
+import rootReducer from "./reducers" //根reducers
 
+import createSagaMiddleware from "redux-saga"//引入saga中间件
 
-export default createStore(rootReducer, composeWithDevTools())
+let sagaMiddleware = createSagaMiddleware()
+
+import helloSaga from "./sagas/Product"
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(helloSaga)
